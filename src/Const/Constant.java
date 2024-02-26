8a04f799b142a3ec87f807c4e833c04ccc41987b
unknown <charlieswires@gmail.com>
2024-02-26 22:28:24 +0000

    tested on mobile hotspot with fixed IP4 address

--- src/Const/Constant.java
+++ src/Const/Constant.java
@@ -2,7 +2,9 @@ package Const;
 
 public class Constant {
 
-	public static final String PI_HOME = "http://192.168.1.65";
+//	public static final String PI_HOME = "http://192.168.1.65";
+//	public static final String PI_HOME = "http://192.168.137.214";
+	public static final String PI_HOME = "http://192.168.137.49";
 	public static final String PORT = ":8080";
 	public static final String PATH_PREFIX = "/submarine";
 }
